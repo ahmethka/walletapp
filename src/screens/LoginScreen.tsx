c77d7834b51610040ef41454053280a3603d83c7
ahmethka <ahmethamdikara@gmail.com>
2025-10-28 10:58:32 +0300

    login ekran düzenleme

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -38,9 +38,9 @@ export default function LoginScreen() {
     })();
   }, [setValue]);
 
-  const onDeleteProfile =  async () => {
+  const onDeleteProfile = async () => {
     await AsyncStorage.removeItem('profile');
-    setValue('email' , '');
+    setValue('email', '');
     setProfile(null);
   }
 
@@ -56,7 +56,7 @@ export default function LoginScreen() {
       };
       await AsyncStorage.setItem('profile', JSON.stringify(nextProfile));
 
-      toast.show(`Hoş geldin${nextProfile?.name ? `, ${nextProfile.name}` : ''}!`,'success');
+      toast.show(`Hoş geldin${nextProfile?.name ? `, ${nextProfile.name}` : ''}!`, 'success');
 
     } catch (e: any) {
       Alert.alert('Giriş başarısız', e?.response?.data?.message || e?.message || 'Bilinmeyen hata');
@@ -72,41 +72,42 @@ export default function LoginScreen() {
       style={{ flex: 1, backgroundColor: tokens.colors.bg }}
       behavior={Platform.OS === 'ios' ? 'padding' : undefined}
     >
-      <View style={{ flex: 1, paddingHorizontal: tokens.spacing.lg, paddingTop: tokens.spacing.xl }}>
+      <View style={{ flex: 2, paddingHorizontal: tokens.spacing.lg, paddingTop: tokens.spacing.xl }}>
         <View style={{ flexDirection: 'row', alignItems: 'center', marginBottom: tokens.spacing.lg }}>
-          <Image source={logoSrc} style={{ width: 400 , height: 80, borderRadius: 8 }} />
-          <Text style={{ fontSize: 24, fontWeight: '800', color: tokens.colors.text, marginLeft: 10 }}>
+          <Image source={logoSrc} style={{ width: 400, height: 80, borderRadius: 8 }} />
+        </View>
+
+        <View style={{ alignItems: 'center', marginBottom: tokens.spacing.lg, display: profile ? 'none' : 'flex' }}>
+          <Text style={{ fontSize: 18, fontWeight: '600', color: tokens.colors.text, marginLeft: 10 }}>
             Hoş Geldin!
           </Text>
-           <Text style={{ fontSize: 24, fontWeight: '800', color: tokens.colors.text, marginLeft: 10 }}>
+          <Text style={{ fontSize: 14, fontWeight: '300', color: tokens.colors.text, marginLeft: 10 , paddingTop:10 }}>
             Telefon numaran veya e-posta adresin
             ile giriş yap.
           </Text>
         </View>
 
-        <View style={{ alignItems: 'center', marginBottom: tokens.spacing.lg , display: profile ? 'flex' : 'none' }}>
-          <View style={{ width: 86, height: 86, borderRadius: 43 , backgroundColor: tokens.colors.text }}> 
-            <Text style={{color:tokens.colors.bg , textAlign:'center' , margin:'auto' , fontSize: 30}}>{profile?.name?.slice(0,2).toUpperCase()}</Text>
+        <View style={{ alignItems: 'center', marginBottom: tokens.spacing.lg, display: profile ? 'flex' : 'none' }}>
+          <View style={{ width: 86, height: 86, borderRadius: 43, backgroundColor: tokens.colors.text }}>
+            <Text style={{ color: tokens.colors.bg, textAlign: 'center', margin: 'auto', fontSize: 30 }}>{profile?.name?.slice(0, 2).toUpperCase()}</Text>
           </View>
-          
+
           {!!profile?.name && (
             <Text style={{ marginTop: 8, fontSize: 16, color: tokens.colors.text }}>
               Hoş geldin, <Text style={{ fontWeight: '700' }}>{profile.name}</Text> 👋
             </Text>
           )}
-         <View style={{ alignItems: 'center', marginTop: 8 , display: profile ? 'flex' : 'none'}}>
-          <Pressable onPress={onDeleteProfile}>
-            <Text style={{ color: tokens.colors.accent, fontWeight: '600', textDecorationLine: 'underline' }}>
-              Bu kişi sen değil misin ? 
-            </Text>
-          </Pressable>
-        </View>
+          <View style={{ alignItems: 'center', marginTop: 8, display: profile ? 'flex' : 'none' }}>
+            <Pressable onPress={onDeleteProfile}>
+              <Text style={{ color: tokens.colors.accent, fontWeight: '600', textDecorationLine: 'underline' }}>
+                Bu kişi sen değil misin ?
+              </Text>
+            </Pressable>
+          </View>
         </View>
 
-        {/* FORM */}
-        
-        <View style={{ gap: 10 , marginTop:60 }}>
-          <Text style={{ color: tokens.colors.muted , display: profile ? 'none' :'flex'}}>E-posta</Text>
+        <View style={{ gap: 10, marginTop: 60 }}>
+          <Text style={{ color: tokens.colors.muted, display: profile ? 'none' : 'flex' }}>E-posta</Text>
           <Controller
             control={control}
             name="email"
@@ -124,7 +125,7 @@ export default function LoginScreen() {
                   borderWidth: 1,
                   borderRadius: tokens.radii.lg,
                   padding: tokens.spacing.md,
-                  display: profile ? 'none' :'flex'
+                  display: profile ? 'none' : 'flex'
                 }}
                 autoCapitalize="none"
                 autoCorrect={false}
@@ -165,7 +166,6 @@ export default function LoginScreen() {
           <Button title="Giriş Yap" onPress={handleSubmit(onSubmit)} loading={loading} />
         </View>
 
-        {/* ALT BAĞLANTILAR */}
         <View style={{ alignItems: 'center', marginTop: 8 }}>
           <Pressable onPress={() => navigation.navigate('Register')}>
             <Text style={{ color: tokens.colors.accent, fontWeight: '600', textDecorationLine: 'underline' }}>
